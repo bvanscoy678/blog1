695b27eec120152bf3185c29ae1e42a172b9d233
Brent Van Scoy <bvanscoy678@gmail.com>
2018-09-04 19:47:54 -0500

    updated settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bvanscoy8380@gmail.com'
